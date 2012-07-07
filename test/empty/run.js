7319b6fffaa6c59f26421060362a3b4f31fe96a9
Farrin Reid <blakmatrix@gmail.com>
2012-07-07 02:15:41 -0800

    [fix] path.existsSync was moved to fs.existsSync

--- test/empty/run.js
+++ test/empty/run.js
@@ -4,7 +4,7 @@ var fs = require('fs'),
     exec = require('child_process').exec
 
 // cleanup
-if (path.existsSync(process.cwd() + '/Numbfile')) {
+if (fs.existsSync(process.cwd() + '/Numbfile')) {
   fs.unlinkSync(process.cwd() + '/Numbfile')
 }
 
@@ -13,7 +13,7 @@ exec('../../cli.js', function (error, stdout, stderr) {
   console.log(stderr)
   assert.ok(error !== null,
     'Error executing numb')
-  assert.ok(path.existsSync(process.cwd() + '/Numbfile'),
+  assert.ok(fs.existsSync(process.cwd() + '/Numbfile'),
     'Numbfile wasn\'t created')
 
 })
